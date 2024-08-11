7be257cb55624e67c1b1edff3a1578b1efc11322
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-08-12 00:00:28 +0900

    240812 Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,29 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             appBar: AppBar(),
             body: Container(
+              height: 150,
+              padding: EdgeInsets.all(10),
               child: Row(
                 children: [
                   Image.asset('aws-certified-cloud-practitioner.png', width: 150,),
-                  Container()
+                  Container(
+                    width: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('카메라팝니다', style: TextStyle(),),
+                        Text('금호동 3가'),
+                        Text('7000원'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite),
+                            Text('4'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               )
             )
