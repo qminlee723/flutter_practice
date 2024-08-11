fdef3b5919df5ae3292f6e9e934599a7a53c9fe7
Gyumin Lee <gyumin.q.lee@gmail.com>
2024-08-12 00:17:57 +0900

    240812 Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(),
-            body: Container(
-              height: 150,
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Image.asset('aws-certified-cloud-practitioner.png', width: 150,),
-                  Container(
-                    width: 300,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('카메라팝니다', style: TextStyle(),),
-                        Text('금호동 3가'),
-                        Text('7000원'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite),
-                            Text('4'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              )
+            body: ShopItem()
             )
-        )
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: Text('안녕')
     );
   }
 }
